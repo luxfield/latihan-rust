e8c2292310661ac22ce4e441586711722ae71b58
Reizenu Arfonia <suryaefendi72@gmail.com>
2020-12-20 12:14:10 +0700

    tambah fungsi baru

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // komentar sebaris
+    /* 
+        komentar multi baris
+    */
+    println!(); // membuat baris baru
+    println!("Hello, world!");
+    println!("format {} arguments", "some"); //prints format some arguments
+}
